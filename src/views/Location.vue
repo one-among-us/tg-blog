345667297e57284b7b5625c4b4a1b4ba75efb4df
Hykilpikonna <me@hydev.org>
2022-12-19 15:25:46 -0500

    [+] Map marker

--- src/views/Location.vue
+++ src/views/Location.vue
@@ -1,12 +1,10 @@
 <template>
     <div id="Location">
-        <LMap :zoom="zoom" :center="[f.latitude ?? f.location?.longitude, f.longitude ?? f.location?.longitude]"
-            :minZoom="3" :maxZoom="16">
-            <LTileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                layer-type="base"
-                name="OpenStreetMap"
-            ></LTileLayer>
+        <LMap :zoom="zoom" :center="center" :minZoom="3" :maxZoom="16">
+            <LMarker :lat-lng="marker"/>
+            <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        layer-type="base"
+                        name="OpenStreetMap"/>
         </LMap>
     </div>
 </template>
@@ -15,14 +13,26 @@
 import {Options, Vue} from 'vue-class-component';
 import "leaflet/dist/leaflet.css";
 import "leaflet"
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
 import {Prop} from "vue-property-decorator";
 import {TGLocationFile} from "@/logic";
 
-@Options({components: {LMap, LTileLayer}})
+@Options({components: {LMap, LTileLayer, LMarker}})
 export default class Location extends Vue
 {
     @Prop({required: true}) f: TGLocationFile
+    marker: number[]
+
+    get center()
+    {
+        const f = this.f
+        return [f.latitude ?? f.location?.longitude, f.longitude ?? f.location?.longitude]
+    }
+
+    created()
+    {
+        this.marker = this.center
+    }
 
     zoom = 14
 }
