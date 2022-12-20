51db048993ba392d56ceb8e1b2298ca0778d5e6a
Hykilpikonna <me@hydev.org>
2022-12-20 18:30:48 -0500

    [O] Auto icon import

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,65 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import {ElementPlusResolver} from "unplugin-vue-components/resolvers";
+
+
+const src = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+
+      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+
+        // Auto import icon components
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+
+      dts: path.resolve(src, 'auto-imports.d.ts'),
     }),
+
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ],
+
+      dts: path.resolve(src, 'components.d.ts'),
+    }),
+
+    Icons({
+      autoInstall: true
     }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': src,
     },
   },
   build: {
     lib: {
-      entry: path.resolve(__dirname, './src/index.ts'),
+      entry: path.resolve(src, 'index.ts'),
       name: 'tg-blog',
       formats: ['es'], // adding 'umd' requires globals set to every external module
       fileName: (format) => `tg-blog.${format}.js`,
