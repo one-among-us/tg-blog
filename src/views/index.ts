71a27f98de8d195af771b319e1ce9be50db243f3
Azalea Gui <me@hydev.org>
2023-01-08 23:23:52 -0500

    [U] Release 1.0.11-alpha3

--- src/views/index.ts
+++ src/views/index.ts
@@ -2,11 +2,9 @@ import TgBlog from "./TgBlog.vue"
 
 import AudioPlayer from "./AudioPlayer.vue"
 import FileView from "./FileView.vue"
-import ImageViewer from "./ImageViewer.vue";
-import Location from "./Location.vue";
-import Poll from "./Poll.vue";
 import PostView from "./PostView.vue"
+import ImageViewer from "./ImageViewer.vue";
 
 export {
-  TgBlog, AudioPlayer, FileView, PostView, ImageViewer, Location, Poll
+  TgBlog, AudioPlayer, FileView, PostView, ImageViewer
 }
