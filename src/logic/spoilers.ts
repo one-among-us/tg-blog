1c1447818178ec91c35e195427fd1ab48b77bafb
Azalea Gui <me@hydev.org>
2023-04-21 15:26:36 -0400

    [F] Fix link wrapped in spoiler

--- src/logic/spoilers.ts
+++ src/logic/spoilers.ts
@@ -12,6 +12,19 @@ function findParent(cls: string, el: Element): Element | null
 }
 
 /**
+ * Find the parent element with the given tag, or null if not found
+ *
+ * @param tag
+ * @param el
+ */
+function findParentByTag(tag: string, el: Element): Element | null
+{
+  // noinspection StatementWithEmptyBodyJS
+  while ((el = el.parentElement) && el.tagName.toLowerCase() != tag);
+  return el;
+}
+
+/**
  * Initialize clickable spoilers on the page
  */
 export function initSpoilers()
@@ -22,25 +35,44 @@ export function initSpoilers()
 
   spoilers.forEach(spoiler =>
   {
-    // Toggle all spoilers at once if they are in the same post
-    const post = findParent("post", spoiler)
-    const siblingSpoilers = post.querySelectorAll(".spoiler")
-
     // Already initialized
     if (spoiler.classList.contains("spoiler-init")) return
     spoiler.classList.add("spoiler-init")
 
-    // Add event listener
-    spoiler.addEventListener('click', () => 
-    {
-        spoiler.classList.toggle("spoiler-visible")
-        console.log(`Spoiler clicked: ${spoiler}`);
+    // Toggle all spoilers at once if they are in the same post
+    const post = findParent("post", spoiler)
+    const siblingSpoilers = post.querySelectorAll(".spoiler")
 
-        // Toggle sibling spoilers if they are in the same post
-        if (post) siblingSpoilers.forEach(it =>
-        {
-          if (it != spoiler) it.classList.toggle("spoiler-visible")
-        })
-    })
+    function handleSpoiler()
+    {
+      spoiler.classList.toggle("spoiler-visible")
+      console.log(`Spoiler clicked: ${spoiler}`);
+
+      // Toggle sibling spoilers if they are in the same post
+      if (post) siblingSpoilers.forEach(it =>
+      {
+        if (it != spoiler) it.classList.toggle("spoiler-visible")
+      })
+    }
+
+    // If the spoiler is wrapped in a link, handle link clicks specially
+    const link = findParentByTag("a", spoiler)
+    if (link)
+    {
+      link.addEventListener('click', (e) =>
+      {
+        // If the spoiler is already visible, let the link work as normal
+        if (spoiler.classList.contains("spoiler-visible")) return
+
+        // If not, treat the click as a spoiler click
+        handleSpoiler()
+        e.preventDefault()
+      })
+
+      return
+    }
+
+    // Add event listener
+    spoiler.addEventListener('click', handleSpoiler)
   })
 }
